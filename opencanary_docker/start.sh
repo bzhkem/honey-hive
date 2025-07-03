d6094ddd03e8a7c761ad05333095ff77a8c15ede
bzhkem <pl.leguengeffroy@gmail.com>
2025-07-03 16:28:26 +0200

    wrong format fix

--- opencanary_docker/start.sh
+++ opencanary_docker/start.sh
@@ -10,7 +10,7 @@ rsyslogd &
   while true; do
     echo "[scanport] Starting scanport.py ..."
     python /scanport.py ...
-    echo "opencanary: {\"dst_host\": \"\", \"dst_port\": -1, \"logtype\": 9999, \"node_id\": \"$(hostname)\", \"src_host\": \"\", \"logdata\": {\"msg\": \"[scanport] scanport.py a crashé ou terminé, tentative de restart dans 10min at $(date +'%F %T')\"}}" >> /app/opencanary.log
+    echo "{\"dst_host\": \"\", \"dst_port\": -1, \"logtype\": 9999, \"node_id\": \"$(hostname)\", \"src_host\": \"\", \"logdata\": {\"msg\": \"[scanport] scanport.py a crashé ou terminé, tentative de restart dans 10min at $(date +'%F %T')\"}}" >> /app/opencanary.log
     sleep 600
   done
 ) &
